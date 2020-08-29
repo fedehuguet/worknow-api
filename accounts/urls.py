eb0418e3f63e06658c395b9f07763fc7b8973794
fedehuguet <fede.huguet14@gmail.com>
2020-08-29 15:34:32 -0500

    User CRUD

---
+++ accounts/urls.py
@@ -0,0 +1,6 @@
+from rest_framework import routers
+
+from accounts.viewsets import UserViewSet
+
+router = routers.DefaultRouter()
+router.register("users", UserViewSet, basename="users")
--- worknow/urls.py
+++ worknow/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from accounts.urls import router as account_router
+
+router = routers.DefaultRouter()
+router.registry.extend(account_router.registry)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Django admin, auth and docs routes
+    # path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/v1/", include(router.urls)),
 ]
